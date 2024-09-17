bd4fb99e4e20931bbc9c22147c9bc4200ca793e0
Guanyu-Lin <guanyul@andrew.cmu.edu>
2024-09-17 11:13:25 -0400

    test

--- test.py
+++ test.py
@@ -16,13 +16,13 @@ labels = torch.LongTensor([[1, 2, 2], [1, 2, 0]])
 # unique_labels, labels_count = labels.unique(dim=0, return_counts=True)
 # res = torch.zeros_like(unique_labels, dtype=torch.float).scatter_add_(0, labels, samples)
 ls = []
-for b, l in zip(samples, labels):
+for sample, label in zip(samples, labels):
     import pdb
     pdb.set_trace()
-    M = torch.zeros(4, b.shape[0])
-    M[l, torch.arange(b.shape[0])] = 1
+    M = torch.zeros(4, sample.shape[0])
+    M[label, torch.arange(sample.shape[0])] = 1
     M = torch.nn.functional.normalize(M, p=1, dim=1)
-    ls.append(torch.mm(M, b))
+    ls.append(torch.mm(M, sample))
 
     
 # M = torch.zeros(samples.shape[0], labels.max()+1, samples.shape[1])
